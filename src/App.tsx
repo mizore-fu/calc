ea0d7aed2679ee25548da9995657a1c48c5516af
mizore-fu <mhxxname0782@gmail.com>
2023-12-18 09:55:16 +0900

    #1 表示部と各ボタンを配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 function App() {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <p>1+1</p>
+      <div>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>+</button>
+        <button>-</button>
+        <button>*</button>
+        <button>/</button>
+        <button>=</button>
+        <button>clear</button>
+        <button>delete</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
